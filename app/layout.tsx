95189b197cf3953722e262e21c362430f380a91b
Aldi <aldifayol@gmail.com>
2026-01-01 07:51:48 +0800

    feat: google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import type React from 'react';
-import '@/app/globals.css';
+import type React from "react";
+import "@/app/globals.css";
 
 export const metadata = {
-  title: 'Tunas Abadi Raya - General Contractor & IT Consultant Services',
+  title: "Tunas Abadi Raya - General Contractor & IT Consultant Services",
   description:
-    'Professional IT consulting services specializing in custom application development for businesses.',
+    "Professional IT consulting services specializing in custom application development for businesses.",
   verification: {
     other: {
-      'facebook-domain-verification': ['r80cdeacxh6o9vf6kdoyf1igpylafx'],
+      "facebook-domain-verification": ["r80cdeacxh6o9vf6kdoyf1igpylafx"],
+      "google-site-verification": ["zfiYbEWGkt5K86nzJ-IpH7MmeM4eqHT8D4dUJaf_7Oc"],
     },
   },
 };
