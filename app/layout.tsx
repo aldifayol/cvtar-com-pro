70043a4d23529ed8a0ce4958610f36147d527150
Aldi <aldifayol@gmail.com>
2026-01-01 20:55:31 +0800

    feat: seo optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,40 @@
 import type React from "react";
+import type { Metadata } from "next";
 import "@/app/globals.css";
 
-export const metadata = {
-  title: "Tunas Abadi Raya - General Contractor & IT Consultant Services",
+export const metadata: Metadata = {
+  title: {
+    default: "Tunas Abadi Raya - General Contractor & IT Consultant Services",
+    template: "%s | Tunas Abadi Raya"
+  }
   description:
     "Professional IT consulting services specializing in custom application development for businesses.",
+  keywords: [
+    "IT consulting, tunasabadiraya, tarofficial, cvtar, software development manado, tunas abadi raya, software house manado, government contractor manado, jasa pembuatan aplikasi manado, jasa pembuatan website manado",
+  ],
   verification: {
     other: {
       "facebook-domain-verification": ["r80cdeacxh6o9vf6kdoyf1igpylafx"],
       "google-site-verification": ["zfiYbEWGkt5K86nzJ-IpH7MmeM4eqHT8D4dUJaf_7Oc"],
     },
+  },
+  openGraph: {
+    title: "Tunas Abadi Raya",
+    description: "IT Consulting based in Manado",
+    url: "https://tunasabadiraya.cv",
+    images: [
+      {
+        url: "https://tunasabadiraya.cv/tar-office.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@ldifayol",
+    images: ["https://tunasabadiraya.cv/tar-office.png"],
   },
 };
 
